93edf99d36f21ca7af709cd296c79339465a56df
Niraj-Aware <92052184+Niraj-Aware@users.noreply.github.com>
2023-08-20 16:11:02 +0530

    Update Stream.py

--- Stream.py
+++ Stream.py
@@ -9,7 +9,7 @@ import tensorflow as tf
 model = load_model('PlantDNet.h5')  # Update with the correct model path
 disease_class = ['Pepper__bell___Bacterial_spot', 'Pepper__bell___healthy', 'Potato___Early_blight', 'Potato___Late_blight', 'Potato___healthy', 'Tomato_Bacterial_spot', 'Tomato_Early_blight', 'Tomato_Late_blight', 'Tomato_Leaf_Mold', 'Tomato_Septoria_leaf_spot', 'Tomato_Spider_mites_Two_spotted_spider_mite', 'Tomato__Target_Spot', 'Tomato__Tomato_YellowLeaf__Curl_Virus', 'Tomato__Tomato_mosaic_virus', 'Tomato_healthy']
 
-def classify_image(img):
+def classify_image(img,model):
     x = image.img_to_array(img)
     x = np.expand_dims(x, axis=0)
     x /= 255
